d291508f78bc5b86bd5a6f6d352ca6a909ab5929
Enzo Amorim <amorimenzo021@gmail.com>
2021-12-07 20:57:58 -0300

    first commit

---
+++ inbox/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWalletProvider(
+    'acoustic kitchen liar define turn cousin father such metal sad correct expect',
+    'https://rinkeby.infura.io/v3/1497829b862840d1bfe52fd7e582a836'
+)
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(abi)
+        .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+        .send({ gas: '1000000', from: accounts[0] });
+
+    console.log('Contract deployed to', result.options.address);
+    provider.engine.stop();
+};
+
+deploy();
